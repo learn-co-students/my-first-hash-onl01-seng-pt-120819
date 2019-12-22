cce2fb2f9d5c07a72b066e46604fd9bb8a9a2536
Alexis Priore <alexispriore@gmail.com>
2019-12-22 20:15:26 +0000

    Done.

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,8 +1,11 @@
-def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
+ my_hash.do
+      expect(my_hash).to be_a(Hash)
+      expect(my_hash.keys.count).to_not eq(0)
 
-  
+my_hash = { “my_name” => “Alexis”, “my_age” => “27” }
+	end
 end
+  
 
 
 def shipping_manifest
